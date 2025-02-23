2e71c23312d5711e418afc8e01c6272be11e763a
CamrynBowWow <bowdencamryn@gmail.com>
2025-02-23 12:28:08 +0200

    Reminder Workflow Done

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 To create project:
 - npx express-generator --no-view --git ./
 
+# Tutorial
+- Youtuber: JavaScript Mastery
+- URL: https://www.youtube.com/watch?v=rOpEN1JDaD0
+
 <br>
 
 # Tech Stack
@@ -14,6 +18,7 @@ To create project:
 - jsonwebtoken
 - bcryptjs
 - Arcjet
+- dayjs
 
 # Info
 
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,10 +1,23 @@
 import Subscription from '../models/subscription.model.js';
+import { workflowClient } from '../config/upstash.js';
+import { SERVER_URL } from '../config/env.js';
 
 export const createSubscription = async (req, res, next) => {
 	try {
 		const subscription = await Subscription.create({ ...req.body, user: req.user._id });
 
-		res.status(201).json({ success: true, data: subscription });
+		const { workflowRunId } = await workflowClient.trigger({
+			url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+			body: {
+				subscriptionId: subscription.id,
+			},
+			headers: {
+				'content-type': 'application/json',
+			},
+			retries: 0,
+		});
+
+		res.status(201).json({ success: true, data: { subscription, workflowRunId } });
 	} catch (error) {
 		next(error);
 	}
@@ -12,7 +25,7 @@ export const createSubscription = async (req, res, next) => {
 
 export const getUserSubscriptions = async (req, res, next) => {
 	try {
-		if (req.user.id != req.params.id) {
+		if (req.user.id !== req.params.id) {
 			const error = new Error('You are not the owner of this account');
 
 			error.status = 401;
